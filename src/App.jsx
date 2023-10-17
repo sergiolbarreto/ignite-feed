84b8edf4362e18757c6dd0a78d5854a4550da2d9
sergiolbarreto <sergioleonardo68@gmail.com>
2023-10-16 23:04:40 -0300

    add state in comments and update post

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,40 @@
+import './global.css'
+
 import { Header } from './components/Header'
 import { Post } from './components/Post'
 import { Sidebar } from './components/Sidebar'
-
 import styles from './App.module.css'
 
-import './global.css'
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/sergiolbarreto.png',
+      name: 'Sérgio Barreto',
+      role: 'Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' }
+    ],
+    publishedAt: new Date('2023-10-15 20:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/sergiolbarreto.png',
+      name: 'Leonardo Barreto',
+      role: 'Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' }
+    ],
+    publishedAt: new Date('2023-10-10 19:00:00')
+  }
+]
 
 export function App() {
 
@@ -15,14 +45,16 @@ export function App() {
      <div className={styles.wrapper}>
       <Sidebar />
       <main>
-        <Post 
-          author="Diego Fernandes" 
-          content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis facilis, doloribus magnam fugiat explicabo ipsam vitae obcaecati repudiandae recusandae error libero aut delectus itaque laboriosam dolorem molestiae? Ut, tempore nesciunt?"
-        />
-        <Post 
-          author="Gabriel Buzzi" 
-          content="Um novo post muito legal"
-        />
+        {posts.map(post => {
+          return (
+            <Post
+              key={post.id}
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+            />
+          );
+        })}
       </main>
 
      </div>
